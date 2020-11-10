8331f8d8e195ecde26c457bd5ab0a5b35e5a05a0
nastya <tinana2013@gmail.com>
2020-11-10 14:08:13 +0500

    Добавьте файлы проекта.

---
+++ laba2OOP/laba2OOP.cpp
@@ -0,0 +1,11 @@
+﻿// laba2OOP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
