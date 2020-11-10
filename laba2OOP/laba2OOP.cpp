f1d40c55f6b5846a00630dfdc563df0aa5806884
nastya <tinana2013@gmail.com>
2020-11-10 19:30:05 +0500

    Создание объекта динамически

--- laba2OOP/laba2OOP.cpp
+++ laba2OOP/laba2OOP.cpp
@@ -11,9 +11,13 @@ public:
 
 int main()
 {
-	Human Artur;
+	setlocale(LC_ALL, "rus");
+	Human Artur;//статически
 	Artur.age = 21;
-	cout << Artur.age;
+	cout <<"возраст Артура "<< Artur.age<<endl;
+	Human* Nastya = new Human;//динамически
+	Nastya->age = 19;
+	cout <<"возраст Насти "<< Nastya->age<<endl;
 }
 
 
