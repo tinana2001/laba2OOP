4c71098f280e6954d0ecab0ac9618d44f8dc5576
nastya <tinana2013@gmail.com>
2020-11-10 16:32:07 +0500

    Создание объекта статически

--- laba2OOP/laba2OOP.cpp
+++ laba2OOP/laba2OOP.cpp
@@ -2,10 +2,18 @@
 //
 
 #include <iostream>
+using namespace std;
+class Human
+{
+public:
+	int age;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Human Artur;
+	Artur.age = 21;
+	cout << Artur.age;
 }
 
 
